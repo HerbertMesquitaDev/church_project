7bd859ea5f57c91c85970906275301261ead148c
Herbert Mesquita <herbertmesquita@gmail.com>
2026-06-15 21:33:54 -0400

    Alterado o settings

--- church_project/settings.py
+++ church_project/settings.py
@@ -210,7 +210,7 @@ EMAIL_PORT          = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS       = str(os.getenv('EMAIL_USE_TLS', 'True')).lower() in ('true', '1')
 EMAIL_HOST_USER     = os.getenv('EMAIL_HOST_USER',     '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL  = os.getenv('DEFAULT_FROM_EMAIL',  'noreply@suaigreja.com.br')
+DEFAULT_FROM_EMAIL  = os.getenv('DEFAULT_FROM_EMAIL',  'noreply@adbrsede.com.br')
 SERVER_EMAIL        = DEFAULT_FROM_EMAIL
 
 
